b46fc9ef7649220b244df6380bb96a41ca0558f0
Your Name <shelbyfayemo@gmail.com>
2021-10-21 21:33:54 -0400

    fixed syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,22 @@
-DROP DATABASE IF EXISTS inventory_db;
-CREATE DATABASE inventory_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
 
-USE inventory_db;
 
 CREATE TABLE departments (
-id INT(12) NOT NULL,
-department_name VARCHAR(30) NOT NULL,
-PRIMARY KEY (id)
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee_role (
-id INT(12) NOT NULL,
-PRIMARY KEY(id),
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
 department_id INT(12) NOT NULL
 );
 
 CREATE TABLE employee(
-    id INT(12) NOT NULL,
-    PRIMARY KEY (id) ,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(12) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
-USE inventory_db;
+USE employees_db;
 
-INSERT INTO departments(department_name) VALUES
-('HR'),
+INSERT INTO departments(department_name) 
+VALUES ('HR'),
 ('Finance'),
 ('IT'),
 ('Marketing');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO employee_role (title, salary, department_id)
 Values ('HR Manager', 70000, 1),
         ('HR Director', 70000, 1),
-        ('Accountant', 80000, 2)
-         ('Financial Advisor', 80000, 2)
-         ('IT Coordinator', 90000, 3)
-         ('Engineer', 91000, 3)
+        ('Accountant', 80000, 2),
+         ('Financial Advisor', 80000, 2),
+         ('IT Coordinator', 90000, 3),
+         ('Engineer', 91000, 3),
          ('Marketing Manager', 70000, 4);
-INSERT INTO employees(first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ('Bailey', 'Moriarty', 1, 1),
         ('Rochelle', 'Reaper', 2, null),
         ('Shelby','Rose', 1, 3),
