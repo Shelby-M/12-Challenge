da8c3b237283b6f9d8126b845adae0a38ffcff4f
Your Name <shelbyfayemo@gmail.com>
2021-10-24 16:51:20 -0400

    query tables

--- server.js
+++ server.js
@@ -15,3 +15,25 @@ console.log("**************************************");
 console.log("* HELLO! WELCOME TO EMPLOYEE MANAGER *");
 console.log("**************************************");
 db.connect();
+
+db.query("SELECT * from departments", function (err, res) {
+  departments = res.map((dep) => ({
+    name: dep.department_name,
+    value: dep.val,
+  }));
+});
+
+db.query("SELECT * from employee_role", function (err, res) {
+  roles = res.map((role) => ({
+    name: role.title,
+    value: role.id,
+  }));
+});
+
+db.query("SELECT * from employee", function (err, res) {
+  employee = res.map((employee) => ({
+    name: `${employee.first_name} ${employee.last_name}`,
+    value: employee.id,
+  }));
+});
+
