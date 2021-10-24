084d9a4af51dcd1e68d29bdf44d14db12d5378f3
Your Name <shelbyfayemo@gmail.com>
2021-10-24 17:11:10 -0400

    start menu created

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 const confirm = require("inquirer-confirm");
+const Choices = require("inquirer/lib/objects/choices");
 const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection({
@@ -36,4 +37,83 @@ db.query("SELECT * from employee", function (err, res) {
     value: employee.id,
   }));
 });
+startMenu();
 
+function startMenu() {
+  inquirer
+    .prompt({
+      name: "choices",
+      type: "list",
+      message: "Please select a task from the menu",
+      choices: [
+        {
+          name: "View Departments",
+          value: "viewDepartments",
+        },
+        {
+          name: "View Employee Roles",
+          value: "viewEmployeeRoles",
+        },
+        {
+          name: "View a list of Employees",
+          value: "viewEmployeeList",
+        },
+        {
+          name: "Add to Departments",
+          value: "addDepartments",
+        },
+        {
+          name: "Add an Employee Role",
+          value: "addEmployeeRole",
+        },
+        {
+          name: "Add a new Employee",
+          value: "addNewEmployee",
+        },
+        {
+          name: "Exit",
+          value: "exit",
+        },
+      ],
+    })
+    .then(function (res) {
+      startMenu(res.choices);
+    });
+}
+
+function menuOptions(answers) {
+  switch (answers) {
+    case "View Departments":
+      viewDepartments();
+      break;
+    case "View Employee Roles":
+      viewEmployeeRoles();
+      break;
+    case "View a list of Employees":
+      viewEmployeeList();
+      break;
+    case "Add to Departments":
+      addDepartments();
+      break;
+    case "Add an Employee Role":
+      addEmployeeRole();
+      break;
+    case "Add a new Employee":
+      addNewEmployee();
+      break;
+    case "Exit":
+      exit();
+  }
+}
+
+function viewDepartments() {}
+
+function viewEmployeeRoles() {}
+
+function viewEmployeeList() {}
+
+function addDepartments() {}
+
+function addEmployeeRole() {}
+
+function exit() {}
